ed7bfc7795bab584badf0535c0fe30cb4c5b56ad
HIST <xx.hist.xx@gmail.com>
2021-03-26 22:49:16 +0200

    fixed testing issue

--- src/test/java/edu/kpi/testcourse/storage/UrlRepositoryFileImplTest.java
+++ src/test/java/edu/kpi/testcourse/storage/UrlRepositoryFileImplTest.java
@@ -1,18 +1,18 @@
 package edu.kpi.testcourse.storage;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import edu.kpi.testcourse.entities.UrlAlias;
 import edu.kpi.testcourse.logic.UrlShortenerConfig;
 import edu.kpi.testcourse.serialization.JsonToolJacksonImpl;
+import java.io.IOException;
+import java.nio.file.Files;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
-import java.nio.file.Files;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-
-class UrlRepositoryFileImplTest {
+public class UrlRepositoryFileImplTest {
 
   UrlShortenerConfig appConfig;
   UrlRepository urlRepository;
@@ -21,8 +21,8 @@ class UrlRepositoryFileImplTest {
   void setUp() {
     try {
       appConfig = new UrlShortenerConfig(
-        Files.createTempDirectory("url-repository-file-test"));
-      Files.write(appConfig.storageRoot().resolve("url-repository.json"), "{}".getBytes());
+        Files.createTempDirectory("alias-repository-file-test"));
+      Files.write(appConfig.storageRoot().resolve("alias-repository.json"), "{}".getBytes());
     } catch (IOException e) {
       e.printStackTrace();
       throw new RuntimeException(e);
@@ -33,7 +33,7 @@ class UrlRepositoryFileImplTest {
   @AfterEach
   void tearDown() {
     try {
-      Files.delete(appConfig.storageRoot().resolve("url-repository.json"));
+      Files.delete(appConfig.storageRoot().resolve("alias-repository.json"));
       Files.delete(appConfig.storageRoot());
     } catch (IOException e) {
       e.printStackTrace();
@@ -50,6 +50,5 @@ class UrlRepositoryFileImplTest {
     // THEN
     assertThat(urlRepository.findUrlAlias("http://r.com/short")).isEqualTo(alias);
   }
-
 
 }
